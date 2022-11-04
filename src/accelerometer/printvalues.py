a54857a070e8cd4f1fe134c8fcab407f4a193794
Amos Yu <yua25@mcmaster.ca>
2022-11-03 23:12:30 -0400

    [#138] Display accelerations in user-friendly plot

--- src/accelerometer/printvalues.py
+++ src/accelerometer/printvalues.py
@@ -1,11 +1,43 @@
-import time
+import adafruit_adxl34x
 import board
 import busio
-import adafruit_adxl34x
+import datetime
+import matplotlib.pyplot as plt
 
 i2c = busio.I2C(board.SCL, board.SDA)
 accelerometer = adafruit_adxl34x.ADXL345(i2c)
 
+x = []
+y = []
+z = []
+t = []
+
+xline, = plt.plot(t, x, 'r', label="x")
+yline, = plt.plot(t, y, 'b', label="y")
+zline, = plt.plot(t, z, 'g', label="z")
+ax = plt.gca()
+plt.legend(loc="upper left")
+plt.title("Accelerometer readings")
+plt.xlabel("Time")
+plt.ylabel("Acceleration (m^2/s)")
+
 while True:
-	print("%f %f %f"%accelerometer.acceleration)
-	time.sleep(1)
+	acceleration = accelerometer.acceleration
+
+	if len(t) > 50:
+		x.pop(0)
+		y.pop(0)
+		z.pop(0)
+		t.pop(0)
+	x.append(acceleration[0])
+	y.append(acceleration[1])
+	z.append(acceleration[2])
+	t.append(datetime.datetime.now())
+
+	xline.set_data(t, x)
+	yline.set_data(t, y)
+	zline.set_data(t, z)
+	ax.relim()
+	ax.autoscale_view()
+	plt.draw()
+	plt.pause(0.1)
