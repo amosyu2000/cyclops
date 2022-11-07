b2e07185349c0389a2b1cc19ed95f69191cf0e23
Brian Le <briantule3@gmail.com>
2022-11-07 12:20:40 -0500

    Toggle LED

--- src/object_detect/TFLite_detection_webcam_loop.py
+++ src/object_detect/TFLite_detection_webcam_loop.py
@@ -224,6 +224,10 @@ try:
 
                         # Draw label
                         object_name = labels[int(classes[i])] # Look up object name from "labels" array using class index
+                        if object_name == 'car':
+                            GPIO.output(27, True)
+                        else:
+                            GPIO.output(27, False)
                         label = '%s: %d%%' % (object_name, int(scores[i]*100)) # Example: 'person: 72%'
                         labelSize, baseLine = cv2.getTextSize(label, cv2.FONT_HERSHEY_SIMPLEX, 0.7, 2) # Get font size
                         label_ymin = max(ymin, labelSize[1] + 10) # Make sure not to draw label too close to top of window
