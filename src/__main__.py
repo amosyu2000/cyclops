2d2930cf30757013c1453873938738342eefec3a
Manuel Lemos <mannylemos@outlook.com>
2023-01-18 23:09:21 -0500

    [#205] Update Recording Software

--- src/__main__.py
+++ src/__main__.py
@@ -1,9 +1,15 @@
 import threading
 import accelerometer.start as Accelerometer
 import ultrasonic_sensor.start as Ultrasonic
+import recording.start as Recording
+
+isCrashed = False # parameter will be updated when a crash is detected 
 
 accelerometer_thread = threading.Thread(name="accelerometer", target=Accelerometer.Start)
 accelerometer_thread.start()
 
 ultrasonic_thread = threading.Thread(name="ultrasonic", target=Ultrasonic.Start)
 ultrasonic_thread.start()
+
+recording_thread = threading.Thread(name="recording", target=lambda: Recording.Start(isCrashed))
+recording_thread.start()
