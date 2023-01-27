b1c95e9e97fd3ff6d5c4fc0e794b76c193fd4239
Brian Le <briantule3@gmail.com>
2023-01-27 13:38:01 -0500

    Created unit test files

---
+++ test/ultrasonic_test.py
@@ -0,0 +1,7 @@
+import sys
+sys.path.append('../src')
+import pytest 
+from ultrasonic_sensor.ultrasonic import Ultrasonic
+
+def test_ultrasonic():
+    assert True
---
+++ test/video_buffer_test.py
@@ -0,0 +1,7 @@
+import sys
+sys.path.append('../src')
+import pytest 
+from recording.video_buffer import Buffer
+
+def test_buffer():
+    assert True
