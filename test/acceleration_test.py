1fe1ee7729097337e7e105636cdeafb60db1641e
Brian Le <briantule3@gmail.com>
2023-01-27 13:38:01 -0500

    Created unit test files

---
+++ test/acceleration_test.py
@@ -0,0 +1,20 @@
+import sys
+sys.path.append('../src')
+import pytest 
+import numpy as np
+import time
+from accelerometer.acceleration_plot import Acceleration_Plot
+
+
+def test_data_points():
+    acceleration_plot = Acceleration_Plot(data_points=50, average_of=3)
+    assert len(acceleration_plot.avg_norm) == 0
+    acceleration_plot.avg_norm.append(5)
+    assert len(acceleration_plot.avg_norm) == 1
+
+    acceleration_plot.avg_norm = np.arange(50).tolist()
+    acceleration_plot.t = np.arange(50).tolist()
+    assert len(acceleration_plot.avg_norm) == 50
+
+def test_crash_detect():
+    assert True
